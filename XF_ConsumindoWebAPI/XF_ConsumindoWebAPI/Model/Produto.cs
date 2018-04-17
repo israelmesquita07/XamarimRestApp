ee6359e97fea28b5877c1c318dc52c2a8f38462e
israelmesquita07 <israelmesquita07@gmail.com>
2018-04-17 18:31:25 -0300

    Initial Commit

---
+++ XF_ConsumindoWebAPI/XF_ConsumindoWebAPI/Model/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF_ConsumindoWebAPI.Model
+{
+    public class Produto
+    {
+       public int Id { get; set; }
+       public string Nome { get; set; }
+       public string Categoria { get; set; }
+       public decimal Preco { get; set; }
+
+    }
+}
---
+++ XF_ConsumindoWebAPI/XF_ConsumindoWebAPI/Service/DataService.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using XF_ConsumindoWebAPI.Model;
+
+
+namespace XF_ConsumindoWebAPI.Service
+{
+    public class DataService
+    {
+        HttpClient client = new HttpClient();
+        public async Task<List<Produto>> GetProdutosAsync()
+        {
+            try
+            {
+                string url = "http://www.macwebapi.somee.com/api/produtos/";
+                var response = await client.GetStringAsync(url);
+                var produtos = JsonConvert.DeserializeObject<List<Produto>>(response);
+                return produtos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task AddProdutoAsync(Produto produto)
+        {
+            try
+            {
+                string url = "http://www.macwebapi.somee.com/api/produtos/{0}";
+                var uri = new Uri(string.Format(url, produto.Id));
+                var data = JsonConvert.SerializeObject(produto);
+                var content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = null;
+                response = await client.PostAsync(uri, content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Erro ao incluir produto");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task UpdateProdutoAsync(Produto produto)
+        {
+            string url = "http://www.macwebapi.somee.com/api/produtos/{0}";
+            var uri = new Uri(string.Format(url, produto.Id));
+            var data = JsonConvert.SerializeObject(produto);
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = null;
+            response = await client.PutAsync(uri, content);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Erro ao atualizar produto");
+            }
+        }
+
+        public async Task DeletaProdutoAsync(Produto produto)
+        {
+            string url = "http://www.macwebapi.somee.com/api/produtos/{0}";
+            var uri = new Uri(string.Format(url, produto.Id));
+            await client.DeleteAsync(uri);
+        }
+
+    }
+}
